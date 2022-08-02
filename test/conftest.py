ab868e724adef6811a92c9852af3ced39c002c74
s.danilin <danilinekb@gmail.com>
2022-08-02 14:18:28 +0300

    Fixtures & conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,2 +1,11 @@
 '''Setting global fixture'''
 
+from pytest import fixture
+
+@fixture
+def ultimate_amswer():
+    return 42
+
+@fixture
+def new_answer(ultimate_amswer):
+    return ultimate_amswer + 1
--- test/test_with_fixture.py
+++ test/test_with_fixture.py
@@ -1,18 +1,14 @@
-from pytest import fixture
-
-@fixture
-def ultimate_amswer():
-    return 42
-
-@fixture
-def new_answer(ultimate_amswer):
-    return ultimate_amswer + 1
-
 def test_get_answer(ultimate_amswer):
     assert 42 == ultimate_amswer
+
 
 def test_get_new_answer(new_answer):
     assert 43 == new_answer
 
+
 def test_get_answers(ultimate_amswer, new_answer):
     assert 42 + 43 == ultimate_amswer + new_answer
+
+
+def test_native(request):
+    assert 'test_with_fixture.py' == request.fspath.basename
