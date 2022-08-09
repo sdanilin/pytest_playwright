b3ca37e9a02c405cc9cffc52862641480eeb2560
s.danilin <danilinekb@gmail.com>
2022-08-09 18:41:09 +0300

    settings.py:BASE_URL;USER

--- conftest.py
+++ conftest.py
@@ -1,15 +1,16 @@
 '''Setting global fixture'''
 
 from pytest import fixture
-from playwright.sync_api import Playwright, sync_playwright, expect
-from page_objects.application import App
+from playwright.sync_api import sync_playwright
+from page_objects.App import App
+import settings
 
 
-fixture(autouse=True, scope='function')
+fixture(autouse=True, scope='session')
 def precondition():
-    print('setup preconditions state')
+    print('hello')
     yield
-    print('setup postconditions sate')
+    print('world')
 
 @fixture(scope='session')
 def get_playwright():
@@ -19,14 +20,13 @@ def get_playwright():
 
 @fixture(scope='session')
 def desktop_app(get_playwright):
-    app = App(get_playwright, base_url='http://127.0.0.1:8000')
+    app = App(get_playwright, base_url=settings.BASE_URL)
     app.goto('/')
     yield app
     app.close()
 
 @fixture(scope='session')
 def desktop_app_auth(desktop_app):
-    app = desktop_app
-    app.goto('/login')
-    app.login('alice', 'Qamania123')
+    desktop_app.app.goto('/login')
+    desktop_app.app.login(**settings.USER)
     yield app
--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -3,8 +3,8 @@
         desktop_app_auth.navegate_to('Create new test')
         desktop_app_auth.create_test(test_name, 'world')
         desktop_app_auth.navegate_to('Test Cases')
-        # assert desktop_app_auth.test_cases.check_test_exists(test_name)
-        # desktop_app_auth.test_cases.delete_test_by_name(test_name)
+        assert desktop_app_auth.test_cases.check_test_exists(test_name)
+        desktop_app_auth.test_cases.delete_test_by_name(test_name)
 
 
     def test_new_testcase_no_descr(self, desktop_app_auth):
@@ -12,8 +12,8 @@
         desktop_app_auth.navegate_to('Create new test')
         desktop_app_auth.create_test(test_name, '')
         desktop_app_auth.navegate_to('Test Cases')
-        # assert desktop_app_auth.test_cases.check_test_exists(test_name)
-        # desktop_app_auth.test_cases.delete_test_by_name(test_name)
+        assert desktop_app_auth.test_cases.check_test_exists(test_name)
+        desktop_app_auth.test_cases.delete_test_by_name(test_name)
 
 
     def test_new_testcase_digits_name(self, desktop_app_auth):
@@ -21,5 +21,5 @@
         desktop_app_auth.navegate_to('Create new test')
         desktop_app_auth.create_test(test_name, 'world')
         desktop_app_auth.navegate_to('Test Cases')
-        # assert desktop_app_auth.test_cases.check_test_exists(test_name)
-        # desktop_app_auth.test_cases.delete_test_by_name(test_name)
+        assert desktop_app_auth.test_cases.check_test_exists(test_name)
+        desktop_app_auth.test_cases.delete_test_by_name(test_name)
